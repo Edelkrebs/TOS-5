eda00576f5532991b6fb5074b7cdbfdad4ac365f
Edelkrebs <edelkrebstrolls@gmail.com>
2021-04-02 11:31:37 +0200

    Paging now maybe works

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -24,13 +24,24 @@ void kmain(struct multiboot_info* mboot_info)
 	populate_bitmap(multiboot);
 
 	init_vmm();
-	identity_map((void*) 0, 0x64, 3);
 
 	/*for(int i = 0; i < round_up(kernel_size, PAGE_SIZE) / PAGE_SIZE; i++){
 		map_page((void*)(KERNEL_OFFSET + i * PAGE_SIZE), (void*)((uint32_t)&_kernel_start + i * PAGE_SIZE), 0x3);
 	}*/
 
-	map_page((void*) 0x100000, (void*) 0x100000, 3);
+	/*for(int i = 0; i < round_up(kernel_size, PAGE_SIZE) / PAGE_SIZE; i++){
+		map_page((void*)(KERNEL_OFFSET + i * PAGE_SIZE), (void*)((uint32_t)&_kernel_start + i * PAGE_SIZE), 0x3);
+	}*/
+
+	identity_map((void*) 0, 64, 3);
+
+	map_page((void*) 0xC0000000, (void*) 0x100000, 0x3);
+	map_page((void*) 0xC0001000, (void*) 0x101000, 0x3);
+	map_page((void*) 0xC0002000, (void*) 0x102000, 0x3);
+	map_page((void*) 0xC0003000, (void*) 0x103000, 0x3);
+	map_page((void*) 0xC0004000, (void*) 0x104000, 0x3);
+	map_page((void*) 0xC0005000, (void*) 0x105000, 0x3);
+	map_page((void*) 0xC0006000, (void*) 0x106000, 0x3);
 
 	activate_paging((void*)page_directory);
 
--- kernel/lib/mm/vmm.c
+++ kernel/lib/mm/vmm.c
@@ -15,8 +15,10 @@ void init_vmm(){
 }
 
 static void create_pdentry(uint32_t index, void* ptaddr, uint16_t flags){
-	page_directory[index] = ((uint32_t)ptaddr / 4096) << 12 | flags | 1;
-	printhex((uint32_t) ptaddr);
+	if((uint32_t) ptaddr % 4096 != 0){
+		panic("EEE");
+	}
+	page_directory[index] = ((uint32_t)ptaddr & ~0xFFF) | flags | 1;
 }
 
 void* get_paddr(void* vaddr){
@@ -35,20 +37,22 @@ void* get_paddr(void* vaddr){
 
 void map_page(void* vaddr, void* paddr, uint16_t flags){
 
+	if((uint32_t) vaddr % 4096 != 0 || (uint32_t) paddr % 4096 != 0) panic("Non aligned addresses");
+
 	uint32_t pdindex = (uint32_t) vaddr >> 22;
 	uint32_t ptindex = (uint32_t) vaddr >> 12 & 0x3FF;
-
-	if((page_directory[pdindex] & 1) == 0){
-		create_pdentry(pdindex, (void*)((uint32_t)pt_start + 4096 * pdindex), 0x3);		
-	}		
-
+	
 	uint32_t* pt = ((uint32_t*)pt_start) + (0x400 * pdindex);
-
+	if((page_directory[pdindex] & 1) == 0){
+		create_pdentry(pdindex, (void*)pt, 0x3);		
+	}		
+	
 	if((pt[ptindex] & 1) == 1){
 		panic("Trying to allocate a page twice!");
 	}
-
+	
 	pt[ptindex] = ((uint32_t)paddr) | (flags & 0xFFF) | 1;
+	
 }
 
 void identity_map(void* vaddr, uint32_t number_of_pages, uint16_t flags){
